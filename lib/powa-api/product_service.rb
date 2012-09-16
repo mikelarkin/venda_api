79725f9eeb738a410dfdee2ca4bed8e1da219a4d
Zac Williams <zac@zacwilliams.com>
2012-09-16 17:59:55 -0500

    initial ProductService

---
+++ lib/powa-api/product_service.rb
@@ -0,0 +1,16 @@
+class PowaApi::ProductService
+
+  def self.get_published_products(publish_token, opts = {})
+  end
+
+  def self.get_published_products_by_sku(sku_list)
+  end
+
+  def self.get_products(type = nil)
+    # TODO - something, anything
+  end
+
+  def self.get_products_by_sku(sku_list)
+  end
+
+end
