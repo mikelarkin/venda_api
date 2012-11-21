ebd6a8eceed670740ebe045365d2f2fde2ca5d94
Mike Larkin <mjlarkin@gmail.com>
2012-11-20 23:50:06 -0500

    Added get_orders method

--- lib/powa_api/order_service.rb
+++ lib/powa_api/order_service.rb
@@ -15,7 +15,7 @@ module PowaApi
               xml.urn(:FindUpdatedOrdersRequest) do |xml|
                 xml.updatedFrom updated_from.strftime('%FT%T.000')
                 xml.updateTo updated_to.strftime('%FT%T.000') if updated_to
-             end
+              end
             end
           end
         end
@@ -30,20 +30,20 @@ module PowaApi
       client = Savon.client wsdl
 
       begin
-      response = client.request :get_order_details do
-        soap.xml do |xml|
-          xml.soapenv(:Envelope, namespaces) do |xml|
+        response = client.request :get_order_details do
+          soap.xml do |xml|
+            xml.soapenv(:Envelope, namespaces) do |xml|
 
-            header_block(xml)
+              header_block(xml)
 
-            xml.soapenv(:Body) do |xml|
-              xml.urn(:GetOrderDetailsRequest) do |xml|
-                xml.orderNumber order_number
-             end
+              xml.soapenv(:Body) do |xml|
+                xml.urn(:GetOrderDetailsRequest) do |xml|
+                  xml.orderNumber order_number
+                end
+              end
             end
           end
         end
-      end
       rescue Savon::SOAP::Fault => e
         return nil if e.message.include?("Order not found")
         raise e.message
@@ -51,6 +51,19 @@ module PowaApi
 
       response.to_array(:get_order_details_response).first
 
+    end
+
+    # This method invokes find_updated_orders and then calls get_order_details for each order
+    # and returns an array of the results
+    def self.get_orders(start_date, end_date)
+      orders = []
+      order_ids = OrderService.find_updated_orders(start_date, end_date)
+
+      order_ids.each do |order_id|
+        orders << OrderService.get_order_details(order_id)
+      end
+
+      return orders.flatten.compact
     end
 
     private
